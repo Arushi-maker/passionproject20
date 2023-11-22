ea565b5174e4d3d9ce728b3f9019e386255cc554
sumedhaKl <sumedhakamaraju@gmail.com>
2023-11-21 18:34:47 -0800

    first commit

--- _posts/squares.js
+++ _posts/squares.js
@@ -1,5 +1,13 @@
 window.addEventListener('load', function() {
-    var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple'];
+    var colors = [
+        'rgb(255, 0, 0)',   // red
+        'rgb(255, 165, 0)', // orange
+        'rgb(255, 255, 0)', // yellow
+        'rgb(0, 128, 0)',   // green
+        'rgb(0, 0, 255)',   // blue
+        'rgb(75, 0, 130)',  // indigo
+        'rgb(128, 0, 128)'  // purple
+    ];
 
     function getRandomColorIndex() {
         return Math.floor(Math.random() * colors.length);
