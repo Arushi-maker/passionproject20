c0b9e5ce1e359f64b2c24a26b3d17af8ba271a5c
sumedhaKl <sumedhakamaraju@gmail.com>
2023-11-30 17:41:13 -0800

    first commit

--- _posts/squares.js
+++ _posts/squares.js
@@ -1,36 +1,27 @@
 window.addEventListener('load', function() {
-    // Create an array of image sources with associated file paths
-    const imagePaths = [
-        { src: 'redsquare.jpg', path: 'C:\\Users\\venka\\.vscode\\redsquare.jpg' },
-        { src: 'orangesquare.jpg', path: 'C:\\Users\\venka\\.vscode\\orangesquare.jpg' },
-        { src: 'yellowsquare.jpg', path: 'C:\\Users\\venka\\.vscode\\yellowsquare.jpg' },
-        { src: 'greensquare.jpg', path: 'C:\\Users\\venka\\.vscode\\greensquare.jpg' },
-        { src: 'bluesquare.jpg', path: 'C:\\Users\\venka\\.vscode\\bluesquare.jpg' },
-        { src: 'purplesquare.jpg', path: 'C:\\Users\\venka\\.vscode\\purplesquare.jpg' }
+    // Create an array of image sources
+    const imageSources = [
+        "redsquare.jpg",
+        "orangesquare.jpg",
+        "yellowsquare.jpg",
+        "greensquare.jpg",
+        "bluesquare.jpg",
+        "purplesquare.jpg"
     ];
 
-    // Loop through the array and create image elements with links
-    imagePaths.forEach(item => {
-        // Create an anchor element
-        const link = document.createElement("a");
-        link.href = item.path;
-        link.target = "_blank"; // Open link in a new tab or window
+    // Get a random index to select a random image
+    const randomIndex = Math.floor(Math.random() * imageSources.length);
+    const selectedImagePath = imageSources[randomIndex];
 
-        // Create an image element
-        const img = document.createElement("img");
+    // Create an image element
+    const img = document.createElement("img");
 
-        // Set the source attribute
-        img.src = item.src;
+    // Set the source and other attributes
+    img.src = selectedImagePath;
+    img.alt = 'Color Square';
+    img.width = 100;
+    img.height = 100;
 
-        // Set other attributes if needed
-        img.alt = 'Color Square';
-        img.width = 100;
-        img.height = 100;
-
-        // Append the image to the anchor
-        link.appendChild(img);
-
-        // Append the anchor to the body
-        document.body.appendChild(link);
-    });
+    // Append the image to the body
+    document.body.appendChild(img);
 });
