50581efa4ded2026c0e58639522a7a69c4108ef4
sumedhaKl <sumedhakamaraju@gmail.com>
2023-12-01 02:56:39 -0800

    first commit

--- _posts/squares.js
+++ _posts/squares.js
@@ -1,18 +1,4 @@
 window.addEventListener('load', function() {
-    // Create an array of image sources
-    const imageSources = [
-        "redsquare.jpg",
-        "orangesquare.jpg",
-        "yellowsquare.jpg",
-        "greensquare.jpg",
-        "bluesquare.jpg",
-        "purplesquare.jpg"
-    ];
-
-    // Get a random index to select a random image
-    const randomIndex = Math.floor(Math.random() * imageSources.length);
-    const selectedImagePath = imageSources[randomIndex];
-
     // Create an image element
     const img = document.createElement("img");
 
