344cc033e843c141cb64d08143dc26ff662c3b16
sumedhaKl <sumedhakamaraju@gmail.com>
2023-12-18 13:27:02 -0800

    first commit

--- _posts/game.js
+++ _posts/game.js
@@ -18,9 +18,55 @@ function initGame() {
 
   score = 0;
   timeLeft = 120;
-  isGameRunning = true;
+  isGameRunning = false;
   lastTimestamp = null;
   lastMoveTimestamp = 0;
+
+  drawStartScreen();
+}
+
+function drawStartScreen() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#000";
+  ctx.font = "40px Arial";
+  ctx.fillText("Click 'Start Game' to begin", canvas.width / 2 - 250, canvas.height / 2 - 20);
+
+  const startButtonWidth = 200;
+  const startButtonHeight = 50;
+  const startButtonX = canvas.width / 2 - startButtonWidth / 2;
+  const startButtonY = canvas.height / 2 + 20;
+
+  ctx.fillStyle = "#00FF00";
+  ctx.fillRect(startButtonX, startButtonY, startButtonWidth, startButtonHeight);
+
+  ctx.fillStyle = "#000";
+  ctx.font = "20px Arial";
+  ctx.fillText("Start Game", startButtonX + 50, startButtonY + 30);
+
+  canvas.addEventListener("click", handleStartClick);
+}
+
+function handleStartClick(event) {
+  const rect = canvas.getBoundingClientRect();
+  const mouseX = event.clientX - rect.left;
+  const mouseY = event.clientY - rect.top;
+
+  const startButtonWidth = 200;
+  const startButtonHeight = 50;
+  const startButtonX = canvas.width / 2 - startButtonWidth / 2;
+  const startButtonY = canvas.height / 2 + 20;
+
+  if (
+    mouseX >= startButtonX &&
+    mouseX <= startButtonX + startButtonWidth &&
+    mouseY >= startButtonY &&
+    mouseY <= startButtonY + startButtonHeight
+  ) {
+    canvas.removeEventListener("click", handleStartClick);
+    isGameRunning = true;
+    requestAnimationFrame(gameLoop);
+  }
 }
 
 function draw() {
@@ -31,7 +77,7 @@ function draw() {
     drawScore();
     drawTimer();
   } else {
-    drawGameOver();
+    drawStartScreen();
   }
 }
 
@@ -88,6 +134,8 @@ function handleClick(event) {
       break;
     }
   }
+
+  moveObjects(); // Move objects after every click
 }
 
 function resetObjects() {
@@ -110,25 +158,14 @@ function updateTimer(timestamp) {
     requestAnimationFrame(updateTimer);
   } else {
     isGameRunning = false;
-    draw();
+    drawGameOver();
   }
 }
 
 function gameLoop(timestamp) {
   if (isGameRunning) {
-    // Move objects every three seconds
-    if (timestamp - lastMoveTimestamp > 3000) {
-      moveObjects();
-      lastMoveTimestamp = timestamp;
-    }
-
     updateTimer(timestamp);
     draw();
-    requestAnimationFrame(gameLoop);
-  } else {
-    initGame();
-    isGameRunning = true;
-    requestAnimationFrame(gameLoop);
   }
 }
 
@@ -141,5 +178,5 @@ function moveObjects() {
 
 canvas.addEventListener("click", handleClick);
 
+// Start the game by initializing it
 initGame();
-gameLoop();
