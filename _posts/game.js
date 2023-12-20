e5298fdb3e8e373f377dd25c1a1b5adc4606a06e
sumedhaKl <sumedhakamaraju@gmail.com>
2023-12-20 14:26:48 -0800

    first commit

--- _posts/game.js
+++ _posts/game.js
@@ -12,15 +12,12 @@ let lastMoveTimestamp = 0;
 let isQuestionnaireCompleted = false;
 
 function startGame() {
-
   handleQuizSubmission();
 }
 
 function handleQuizSubmission() {
   document.getElementById("questionnaire").style.display = "none";
-
   document.getElementById("gameContent").style.display = "block";
-
   initializeGame();
 }
 
@@ -70,72 +67,21 @@ function handleQuestionnaireSubmit() {
 
   // Start the game
   isGameRunning = true;
-  requestAnimationFrame(gameLoop);
-}
 
-    const anxietyAnswers = [...anxietyForm.elements].filter((el) => el.checked).map((el) => el.value);
-    const depressionAnswers = [...depressionForm.elements].filter((el) => el.checked).map((el) => el.value);
+  const anxietyAnswers = [...anxietyForm.elements].filter((el) => el.checked).map((el) => el.value);
+  const depressionAnswers = [...depressionForm.elements].filter((el) => el.checked).map((el) => el.value);
 
-    // Check if all questions are answered
-    if (anxietyAnswers.length === 3 && depressionAnswers.length === 3) {
-        // Process answers (you can add your logic here)
+  // Check if all questions are answered
+  if (anxietyAnswers.length === 3 && depressionAnswers.length === 3) {
+    // Process answers (you can add your logic here)
 
-        // Remove the questionnaire and start the game
-        document.body.removeChild(anxietyForm);
-        document.body.removeChild(depressionForm);
-        initGame();
-        requestAnimationFrame(gameLoop);
-    } else {
-        alert("Please answer all questions before starting the game.");
-    }
+    // Remove the questionnaire and start the game
+    document.body.removeChild(anxietyForm);
+    document.body.removeChild(depressionForm);
     requestAnimationFrame(gameLoop);
-
-// New event listener for questionnaire submit button
-const questionnaireSubmitButton = document.createElement("button");
-questionnaireSubmitButton.textContent = "Submit Questionnaire";
-questionnaireSubmitButton.addEventListener("click", handleQuestionnaireSubmit);
-document.body.appendChild(questionnaireSubmitButton);
-
-function initGame() {
-  // Initialize game objects
-  objects = [
-    { x: 100, y: 100, width: 50, height: 50, color: "#FF0000", isTarget: true },
-    { x: 200, y: 200, width: 50, height: 50, color: "#00FF00", isTarget: false },
-    { x: 300, y: 300, width: 50, height: 50, color: "#0000FF", isTarget: false },
-    { x: 400, y: 400, width: 50, height: 50, color: "#FFA500", isTarget: true }, // Orange square
-    { x: 500, y: 500, width: 50, height: 50, color: "#FFFF00", isTarget: false }, // Yellow square
-    { x: 600, y: 200, width: 50, height: 50, color: "#800080", isTarget: false }, // Purple square
-  ];
-
-  score = 0;
-  timeLeft = 120;
-  isGameRunning = false;
-  lastTimestamp = null;
-  lastMoveTimestamp = 0;
-
-  drawStartScreen();
-}
-
-function drawStartScreen() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  ctx.fillStyle = "#000";
-  ctx.font = "40px Arial";
-  ctx.fillText("Click 'Start Game' to begin", canvas.width / 2 - 250, canvas.height / 2 - 20);
-
-  const startButtonWidth = 200;
-  const startButtonHeight = 50;
-  const startButtonX = canvas.width / 2 - startButtonWidth / 2;
-  const startButtonY = canvas.height / 2 + 20;
-
-  ctx.fillStyle = "#00FF00";
-  ctx.fillRect(startButtonX, startButtonY, startButtonWidth, startButtonHeight);
-
-  ctx.fillStyle = "#000";
-  ctx.font = "20px Arial";
-  ctx.fillText("Start Game", startButtonX + 50, startButtonY + 30);
-
-  canvas.addEventListener("click", handleStartClick);
+  } else {
+    alert("Please answer all questions before starting the game.");
+  }
 }
 
 function draw() {
@@ -146,7 +92,7 @@ function draw() {
     drawScore();
     drawTimer();
   } else {
-    drawGameOver();
+    drawStartScreen();
   }
 }
 
@@ -171,69 +117,22 @@ function drawTimer() {
   ctx.fillText(timerText, canvas.width - timerWidth - 10, 30);
 }
 
-function drawGameOver() {
+function drawStartScreen() {
   ctx.fillStyle = "#000";
   ctx.font = "40px Arial";
-  ctx.fillText("Game Over", canvas.width / 2 - 100, canvas.height / 2 - 20);
-  ctx.font = "20px Arial";
-  ctx.fillText(`Your score: ${score}`, canvas.width / 2 - 60, canvas.height / 2 + 20);
-
-  const restartButtonWidth = 200;
-  const restartButtonHeight = 50;
-  const restartButtonX = canvas.width / 2 - restartButtonWidth / 2;
-  const restartButtonY = canvas.height / 2 + 80;
-
-  ctx.fillStyle = "#00FF00";
-  ctx.fillRect(restartButtonX, restartButtonY, restartButtonWidth, restartButtonHeight);
-
-  ctx.fillStyle = "#000";
-  ctx.font = "20px Arial";
-  ctx.fillText("Restart Game", restartButtonX + 30, restartButtonY + 30);
-
-  canvas.addEventListener("click", handleRestartClick);
-}
-
-function handleStartClick(event) {
-  const rect = canvas.getBoundingClientRect();
-  const mouseX = event.clientX - rect.left;
-  const mouseY = event.clientY - rect.top;
+  ctx.fillText("Click 'Start Game' to begin", canvas.width / 2 - 250, canvas.height / 2 - 20);
 
   const startButtonWidth = 200;
   const startButtonHeight = 50;
   const startButtonX = canvas.width / 2 - startButtonWidth / 2;
   const startButtonY = canvas.height / 2 + 20;
 
-  if (
-    mouseX >= startButtonX &&
-    mouseX <= startButtonX + startButtonWidth &&
-    mouseY >= startButtonY &&
-    mouseY <= startButtonY + startButtonHeight
-  ) {
-    isGameRunning = true;
-    canvas.removeEventListener("click", handleStartClick);
-    requestAnimationFrame(gameLoop);
-  }
-}
+  ctx.fillStyle = "#00FF00";
+  ctx.fillRect(startButtonX, startButtonY, startButtonWidth, startButtonHeight);
 
-function handleRestartClick(event) {
-  const rect = canvas.getBoundingClientRect();
-  const mouseX = event.clientX - rect.left;
-  const mouseY = event.clientY - rect.top;
-
-  const restartButtonWidth = 200;
-  const restartButtonHeight = 50;
-  const restartButtonX = canvas.width / 2 - restartButtonWidth / 2;
-  const restartButtonY = canvas.height / 2 + 80;
-
-  if (
-    mouseX >= restartButtonX &&
-    mouseX <= restartButtonX + restartButtonWidth &&
-    mouseY >= restartButtonY &&
-    mouseY <= restartButtonY + restartButtonHeight
-  ) {
-    initGame();
-    requestAnimationFrame(gameLoop);
-  }
+  ctx.fillStyle = "#000";
+  ctx.font = "20px Arial";
+  ctx.fillText("Start Game", startButtonX + 50, startButtonY + 30);
 }
 
 function handleClick(event) {
@@ -294,4 +193,4 @@ function gameLoop(timestamp) {
 canvas.addEventListener("click", handleClick);
 
 // Start the game by initializing it
-initGame();
+initializeGame();
