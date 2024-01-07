b8bec84abce8ecbe61d17c3d3a62a3ab63827031
sumedhaKl <sumedhakamaraju@gmail.com>
2024-01-07 01:54:53 -0800

    first commit

--- _posts/script.js
+++ _posts/script.js
@@ -1,24 +1,38 @@
 var character = document.getElementById("character");
-var block = document.getElementById("block");
+var block1 = document.getElementById("block1");
+var block2 = document.getElementById("block2");
 var counter=0;
+
 function jump(){
-    if(character.classList == "animate"){return}
+    if(character.classList.contains("animate")) {
+        return;
+    }
     character.classList.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
     },1000);
 }
+
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        block.style.display = "none";
-        console.log("Collision detected. Showing alert.");
+    let block1Left = parseInt(window.getComputedStyle(block1).getPropertyValue("left"));
+    let block2Left = parseInt(window.getComputedStyle(block2).getPropertyValue("left"));
+    
+    if(
+        (block1Left<20 && block1Left>-20 && characterTop>=130) ||
+        (block2Left<20 && block2Left>-20 && characterTop>=130)
+    ) {    
+        block1.style.animation = "none";
+        block1.style.display = "none";
+        block2.style.animation = "none";
+        block2.style.display = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
         counter=0;
-        block.style.animation = "block 5s infinite linear";
-    }else{
+        block1.style.animation = "block 5s infinite linear";
+        block2.style.animation = "block 10s infinite linear";
+        block1.style.display = "block";
+        block2.style.display = "block";
+    } else{
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
