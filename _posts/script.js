fc3ea5c7a637a3e4c9924752fad0029eca3b53a3
sumedhaKl <sumedhakamaraju@gmail.com>
2024-01-07 00:43:22 -0800

    first commit

--- _posts/script.js
+++ _posts/script.js
@@ -1,6 +1,5 @@
 var character = document.getElementById("character");
-var block1 = document.getElementById("block1");
-var block2 = document.getElementById("block2");
+var block = document.getElementById("block");
 var counter=0;
 function jump(){
     if(character.classList == "animate"){return;}
@@ -11,20 +10,14 @@ function jump(){
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let block1Left = parseInt(window.getComputedStyle(block1).getPropertyValue("left"));
-    if(block1Left<20 && block1Left>-20 && characterTop>=130){
-        block1.style.animation = "none";
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
         alert("Game over. score: "+Math.floor(counter/100));
         counter=0;
-        block1.style.animation = "block 5s infinite linear";
-    let block2Left = parseInt(window.getComputedStyle(block2).getPropertyValue("left"));
-    if(block2Left<20 && block2Left>-20 && characterTop>=130){
-        block2.style.animation = "none";
-        alert("Game over. score: "+Math.floor(counter/100));
-        counter=0;
-        block2.style.animation = "block 5s infinite linear";
+        block.style.animation = "block 5s infinite linear";
     }else{
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
-}},10);
+},10);
