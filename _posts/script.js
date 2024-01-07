b5468c8e844e9f432addfef804e9f284a34f6f39
sumedhaKl <sumedhakamaraju@gmail.com>
2024-01-07 00:46:43 -0800

    first commit

--- _posts/script.js
+++ _posts/script.js
@@ -8,7 +8,7 @@ function jump(){
         character.classList.remove("animate");
     },300);
 }
-var checkDead = setInterval(function() {
+var checkDead = setInterval(function()) {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
@@ -20,4 +20,6 @@ var checkDead = setInterval(function() {
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
-}, 10);
+};10;
+
+setInterval(checkDead, 10);
