a24f40925870a7d0a387fc8b3202ba3965281167
sumedhaKl <sumedhakamaraju@gmail.com>
2024-01-07 00:24:56 -0800

    first commit

--- _posts/script.js
+++ _posts/script.js
@@ -10,13 +10,8 @@ function jump(){
 }
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let block1Left = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(block1Left<20 && block1Left>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 5s infinite linear";
-    if(block2Left<20 && block2Left>-20 && characterTop>=130){
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
         block.style.animation = "none";
         alert("Game over. score: "+Math.floor(counter/100));
         counter=0;
@@ -25,4 +20,4 @@ var checkDead = setInterval(function() {
         counter++;
         document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
-}},10);
+},10);
