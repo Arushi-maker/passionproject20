b28157f306a347e72ece03c1b8a3f4ed8a987432
sumedhaKl <sumedhakamaraju@gmail.com>
2024-01-06 19:42:51 -0800

    first commit

--- _posts/script.js
+++ _posts/script.js
@@ -1,5 +1,5 @@
 var character = document.getElementById("character");
-document.addEventListener("click",jump);
+document.addEventListener("click", jump, jump);
 function jump(){
     if(character.classList == "animate"){return;}
     character.classList.add("animate");
