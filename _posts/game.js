4ff06ce5be94451f62bfea286e022854403979b8
sumedhaKl <sumedhakamaraju@gmail.com>
2024-01-10 06:26:06 -0800

    first commit

--- _posts/game.js
+++ _posts/game.js
@@ -1,12 +1,28 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
+var myGamePiece;
+var myUpBtn;
+var myDownBtn;
+var myLeftBtn;
+var myRightBtn;
+
 let objects = [];
 let score = 0;
 let timeLeft = 120;
 let isGameRunning = false;
 let lastTimestamp = null;
 let lastMoveTimestamp = 0;
+
+function startCombinedGame() {
+  // Call both start functions
+  startGame();
+  startGame2();
+}
+
+if ("submitQuestionnaire") true; {
+  startGame();
+}
 
 // Additional variable to track whether the questionnaire is completed
 let isQuestionnaireCompleted = false;
@@ -192,3 +208,4 @@ function gameLoop(timestamp) {
 }
 
 canvas.addEventListener("click", handleClick);
+
